f1e588cbbb4a4026fd57b2bdf2d341f02377e906
Sam Schott <ss2151@cam.ac.uk>
2018-09-20 20:32:44 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name="Keithley2600",
       version="0.1.0",
       description="Full Python driver for the Keithley 2600 series.",
-      url="https://github.com/OE-FET/Keithley2600-driver.git",
+      url="https://github.com/OE-FET/Keithley2600.git",
       author="Sam Schott",
       author_email="ss2151@cam.ac.uk",
       licence='MIT',
