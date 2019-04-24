30ceba7f77e4c1ef6dab6348d1ec3433b451a7d4
Sam Schott <ss2151@cam.ac.uk>
2019-04-24 12:47:52 +0100

    simplified plotting

--- keithley2600/sweep_data.py
+++ keithley2600/sweep_data.py
@@ -734,24 +734,17 @@ class TransistorSweepData(ResultTable):
 
     def plot(self, *args, **kwargs):
         """
-        Plots the transfer or output curves. Overrides :meth:`ResultTable.plot`.
+        Plots the transfer or output curves. Overrides :func:`ResultTable.plot`.
         Absolute values are plotted, on a linear scale for output characteristics
         and a logarithmic scale for transfer characteristics. All arguments are passed
-        on to :meth:`ResultTable.plot`.
+        on to :func:`ResultTable.plot`.
         """
-
-        def sqrt_abs(x):
-            return np.sqrt(np.abs(x))
 
         if self.sweep_type == 'transfer':
             fig, ax = super(self.__class__, self).plot(func=np.abs, *args, **kwargs)
             ax.set_yscale('log')
             ax.set_ylabel('I [A]')
 
-            fig, ax = super(self.__class__, self).plot(func=sqrt_abs, *args, **kwargs)
-            ax.set_yscale('linear')
-            ax.set_ylabel('$\\mathregular{I^{1/2}}$ ' +
-                          self.UNIT_FORMAT.format('$\\mathregular{A^{1/2}}$'))
         elif self.sweep_type == 'output':
             fig, ax = super(self.__class__, self).plot(func=np.abs, *args, **kwargs)
             ax.set_yscale('linear')
