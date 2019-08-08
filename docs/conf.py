ee39b5dc28953c68763257e6743d212821d48888
Sam Schott <ss2151@cam.ac.uk>
2019-08-08 14:01:27 +0100

    use custom css theme

--- docs/conf.py
+++ docs/conf.py
@@ -116,7 +116,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -128,6 +128,14 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
+# Override default css with our own theme
+html_context = {
+    'css_files': [
+        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+        '_static/custom.css',
+    ],
+}
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
